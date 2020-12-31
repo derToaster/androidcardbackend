2ab9235847cdb4594029c58478f7596e7b648061
derToaster <ashkanmoradi1412@yahoo.de>
2020-12-31 09:53:44 +0100

    Initial commit

---
+++ src/main/java/com/dertoaster/androidcardbackend/AndroidcardbackendApplication.java
@@ -0,0 +1,13 @@
+package com.dertoaster.androidcardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidcardbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidcardbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dertoaster/androidcardbackend/AndroidcardbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dertoaster.androidcardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidcardbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
